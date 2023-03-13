063fc16ead73e2711bb7d85f2c4a24cd7e946827
allyhere <camilomichelettors@gmail.com>
2023-03-13 14:28:29 -0300

    feat: add typewritter react wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Homepage from '@site/src/components/Homepage';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Homepage from "@site/src/components/Homepage";
 
 function HomepageHeader() {
   return (
-    <header className='homepage'>
-      <div className="homepage-container">
-        <Homepage />
-      </div>
+    <header className="homepage">
+      <Homepage />
     </header>
   );
 }
@@ -17,8 +15,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      description="Aprenda sobre DevOps, de comunidade para comunidade">
+    <Layout description="Aprenda sobre DevOps, de comunidade para comunidade">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
